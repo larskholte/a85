f0abe604aa6d811fa1895831101fde71e04dcd81
Lars Holte <larskholte@Lars-Holtes-MacBook-Pro.local>
2018-08-24 14:41:08 -0700

    Correct capitalization

--- a85.h
+++ a85.h
@@ -10,7 +10,7 @@ typedef uint32_t u32;
 // Returns size of buffer required for to_a85 function.
 int size_for_a85(int binlen, bool append_null = false);
 
-// Translates the given binary data of the given size to ascii85.
+// Translates the given binary data of the given size to Ascii85.
 // Can translate in-place.
 // Optionally appends a null character.
 void to_a85(const u8* data, int binlen, char* text, bool append_null = false);
