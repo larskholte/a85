e9572b81ab279a65e49e8c9701905c7522a20205
Lars Holte <larskholte@Lars-Holtes-MacBook-Pro.local>
2019-07-08 20:11:15 -0700

    Added comments

--- a85.cpp
+++ a85.cpp
@@ -9,11 +9,14 @@ int size_for_a85(int binlen, bool append_null) {
 }
 
 void to_a85(const u8* data, int binlen, char* text, bool append_null) {
+    // Go to end of data and text buffers
     data += binlen;
     text += size_for_a85(binlen,append_null);
+    // Append null if requested
     if (append_null) {
         *(--text) = 0;
     }
+    // If number of bytes is not divisible by 4, act as if null bytes were added to end of buffer
     int rem = binlen & 3;
     if (rem) {
         u32 val = 0;
@@ -31,10 +34,12 @@ void to_a85(const u8* data, int binlen, char* text, bool append_null) {
         binlen &= ~3;
     }
     while (binlen) {
+        // Process chunks of 4 bytes as 32-bit values
         u32 val = *(--data);
         val |= *(--data) << 8;
         val |= *(--data) << 16;
         val |= *(--data) << 24;
+        // Convert to base 85
         *(--text) = val % 85 + 33;
         val /= 85;
         *(--text) = val % 85 + 33;
@@ -55,8 +60,9 @@ int size_for_bin(int textlen) {
 void from_a85(const char* text, int textlen, u8* data) {
     while (textlen) {
         if (textlen < 5) {
+            // Determine represented value in base 85
             u32 val = 0;
-            int factor = 52200625;
+            int factor = 52200625; // 85^4
             int i;
             for (i = 0; i < textlen; i++) {
                 val += (*(text++) - 33) * factor;
@@ -73,11 +79,13 @@ void from_a85(const char* text, int textlen, u8* data) {
             }
             break;
         }
-        u32 val = (*(text++) - 33) * 52200625;
-        val += (*(text++) - 33) * 614125;
-        val += (*(text++) - 33) * 7225;
-        val += (*(text++) - 33) * 85;
-        val += (*(text++) - 33);
+        // Determine represented value in base 85
+        u32 val = (*(text++) - 33) * 52200625; // 85^4
+        val += (*(text++) - 33) * 614125; // 85^3
+        val += (*(text++) - 33) * 7225; // 85^2
+        val += (*(text++) - 33) * 85; // 85^1
+        val += (*(text++) - 33); // 85^0
+        // Write out in big-endian order
         *(data++) = val >> 24;
         *(data++) = val >> 16;
         *(data++) = val >> 8;
